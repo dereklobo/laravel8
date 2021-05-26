648ba9f539524b52eda1c5889f5038d008de9a57
Derek Lobo <dereklobo89@gmail.com>
2021-05-26 08:41:54 -0400

    cache and arrow function

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,15 @@ Route::get('posts', function () {
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__."/../resources/posts/{$slug}.html";
-
-    if(! file_exists($path)){
+    if(! file_exists($path = __DIR__."/../resources/posts/{$slug}.html")){
         return redirect('/');
     }
 
-    $post = file_get_contents($path);
+
+    $post = cache()->remember("posts.{$slug}", now()->day(), fn() => file_get_contents($path));
+
     return view('post', [
         'post' => $post
     ]);
+    
 })->where('post', '[A-z_\-]+');
