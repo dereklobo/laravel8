eb163202fb0af952ab19376eda0a52f8577f10f3
Derek Lobo <dereklobo89@gmail.com>
2021-06-01 06:44:48 -0400

    using slug and yaml front matter

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,19 +7,21 @@
     <title>Document</title>
 </head>
 <body>
-    <h1><a href="/posts/my-first-post"> My First Post</a></h1>
 
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
-
-
-    <h1><a href="posts/my-second-post"> My Second Post</a></h1>
-
-
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
+    <div class="container">
+        @foreach ($posts as $post)
+            <article>
+                <h1>
+                    <a href="/posts/{{$post->slug}}">{{$post->title}}</a>
+                </h1>
+                
+                <div>
+                    {{$post->excerpt}}
+                </div>
+            </article>
+        @endforeach
+    </div>
+    
 
 </body>
 </html>
